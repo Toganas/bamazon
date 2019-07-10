118218e546461ffbfe96c145486c321295280427
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-10 15:07:16 -0500

    added a connection to mysql.  Added inquirer prompts to the js.  If it's not a number, then it returns an error message, otherwise, it continues to the next question.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,4 +13,44 @@ const conn = mysql.createConnection(options);
 
 conn.connect(function (err) {
     if (err) throw err;
+    console.log("connected");
+    purchase();
 });
+
+purchase = () => {
+    inquirer.prompt([
+        {
+            name: "productID",
+            message: "What is the ID of the product you would like to purchase?",
+            validate: (value) =>{
+                if (isNaN(value) === false){
+                    return true;
+                } else {
+                    console.log("\nProduct ID's must be a number");
+                }
+            }
+        },
+        {
+            name: "amount",
+            message: "How much would you like to purchase?",
+            validate: (value) => {
+                if (isNaN(value) === false) {
+                    return true;
+                } else {
+                    console.log("\nThe amount you purchase must be a number");
+                }
+            }
+        }
+    ]).then((buying) => {
+        let product = buying.productID;
+        console.log(product);
+        // let amount = buying.amount;
+        // let sql = "SELECT * FROM products WHERE product = ?"
+        // conn.query(sql, product, (err,res) => {
+        //     if (err) throw err;
+        //     console.table(res);
+        // })
+
+    })
+    conn.end();
+}
