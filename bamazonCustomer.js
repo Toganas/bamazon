bf8b205fc98a01a1087b4261695e963ddd45e8ad
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-07-11 21:52:09 -0500

    been a while, here's the update of updates.  Created the displayInventory function using inquirer to ask if people wanted to see the mysql database and the information in it.  clarified ID to item_id in question productID. Created variables for buying.amount and buying.productID.  created a query that shows how much a person is trying to purchase of what item and for how much each.  a console.log says that there's not enough product if the amount is more than what is in the stock_quantity in the database.  Otherwise, it tells you how much you paid in order to purchase the item.  however, the database doesn't update the new quanitity, so trying to fix that.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
-const mysql = require ("mysql");
-const inquirer = require ("inquirer");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
 const options = ({
     host: "localhost",
@@ -14,19 +14,41 @@ const conn = mysql.createConnection(options);
 conn.connect(function (err) {
     if (err) throw err;
     console.log("connected");
-    purchase();
+    displayInventory();
 });
+
+displayInventory = () => {
+    inquirer.prompt([
+        {
+            name: "displayInv",
+            message: "Would you like to look at the inventory first?",
+            type: "list",
+            choices: ["Yes", "No"]
+        }
+    ]).then((answer) => {
+        if (answer.displayInv === "Yes") {
+            conn.query("SELECT * FROM products", (err, res) => {
+                if (err) throw err;
+                console.table(res);
+                purchase();
+            })
+        } else {
+            purchase();
+        }
+    }
+    )
+}
 
 purchase = () => {
     inquirer.prompt([
         {
             name: "productID",
-            message: "What is the ID of the product you would like to purchase?",
-            validate: (value) =>{
-                if (isNaN(value) === false){
+            message: "What is the item_ID of the product you would like to purchase?",
+            validate: (value) => {
+                if (isNaN(value) === false) {
                     return true;
                 } else {
-                    console.log("\nProduct ID's must be a number");
+                    console.log("\nitem_ID must be a number");
                 }
             }
         },
@@ -42,15 +64,51 @@ purchase = () => {
             }
         }
     ]).then((buying) => {
-        let product = buying.productID;
-        console.log(product);
-        // let amount = buying.amount;
-        // let sql = "SELECT * FROM products WHERE product = ?"
-        // conn.query(sql, product, (err,res) => {
-        //     if (err) throw err;
-        //     console.table(res);
-        // })
+        let amount = buying.amount;
+        let product = buying.productID
+        // console.log(amount);
+        conn.query("SELECT product_name, department_name, price, stock_quantity FROM products WHERE ?", { item_id: product }, (err, res) => {
+            if (err) throw err;
+            console.log(`You are trying to purchase ${amount} of ${res[0].product_name} from ${res[0].department_name} at a cost of $${res[0].price} each`);
+            if (res[0].stock_quantity >= amount) {
+                let newQuantity = res[0].stock_quantity - amount
+                conn.query("UPDATE products SET ? WHERE ?"),
+                    [{
+                        stock_quantity: newQuantity
+                    }, {
+                        item_id: product
+                    }]
+                let cost = amount * res[0].price
+                console.log(`Congratulations, you have purchased ${amount} of ${res[0].product_name} for a total of $${cost}`);
+                shopping();
+            } else {
+                console.log("Sorry, we don't have enough inventory to fulfill your request!");
+                shopping();
+            }
 
+
+
+            // conn.query("SELECT stock_quantity FROM products")
+            // conn.query("UPDATE products SET ? WHERE ?")
+
+        });
     })
-    conn.end();
+
+}
+
+shopping = () => {
+    inquirer.prompt([
+        {
+            name: "shop",
+            message: "Would you like to continue shopping?",
+            type: "list",
+            choices: ["Yes", "No"]
+        }
+    ]).then((answer) => {
+        if (answer.shop === "Yes") {
+            displayInventory();
+        } else {
+            conn.end();
+        }
+    })
 }
